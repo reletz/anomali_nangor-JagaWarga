2a7aac5658caefd676f5d2cf7e24e2c69bb356bb
reletz <naufarrelzhafifabhista@gmail.com>
2026-01-04 13:14:34 +0700

    fix: not autorunning ecto.create for unit test compability

--- src/services/report/test/test_helper.exs
+++ src/services/report/test/test_helper.exs
@@ -1,2 +1,6 @@
 ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(Report.Repo, :manual)
+
+# Only setup sandbox mode if Repo is started (for integration tests)
+if Application.get_env(:report, :start_repo, true) do
+  Ecto.Adapters.SQL.Sandbox.mode(Report.Repo, :manual)
+end
